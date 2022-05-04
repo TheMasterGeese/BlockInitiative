c83f3ddf6d99050fd1450abd272c239d731ed14b
TheMasterGeese <themastergeese@gmail.com>
2022-05-04 09:08:37 -0400

    holding off until I can hopefully get access to the pf2e types

--- src/mg-block-initiative.ts
+++ src/mg-block-initiative.ts
@@ -2,9 +2,9 @@
 // Ready-Check Module
 // Register Game Settings
 Hooks.once("init", function () {
-    game.settings.register("block-initiative", "showChatMessagesForUserUpdates", {
-        name: game.i18n.localize("BLOCKINITIATIVE.SettingsChatMessagesForUserUpdatesTitle"),
-        hint: game.i18n.localize("BLOCKINITIATIVE.SettingsChatMessagesForUserUpdatesHint"),
+    game.settings.register("mg-block-initiative", "EnableInitiativeHandicap", {
+        name: game.i18n.localize("BLOCKINITIATIVE.SettingsEnableInitiativeHandicapTitle"),
+        hint: game.i18n.localize("BLOCKINITIATIVE.SettingsEnableInitiativeHandicapHint"),
         scope: "world",
         config: true,
         default: true,
@@ -12,106 +12,8 @@ Hooks.once("init", function () {
     });
 });
 
-// Block-Initiative Module
-// If the appropriate conditions are met, send a message over discord informing players it is their turn.
-async function checkStatusForMessages() {
-    let text;
-    if (game.user.hasPlayerOwner && AreUsersReady(false)) {
-        text = game.i18n.localize("BLOCKINITIATIVE.ResolvePlayerActionsMessage");
-    } else if (AreUsersReady(true)) {
-        text = game.i18n.localize("BLOCKINITIATIVE.PlayersReactMessage");
-    } else {
-        return;
-    }
-    let userList = await getUserDiscordIDs(game.user.hasPlayerOwner)
-    let message = buildMessage(userList, text)
-    sendDiscordMessage(message);
-}
+Hooks.on("updateCombatant", function(combatant : any, data : { flags : any, initiative : number, _id : string } ) {
 
-// Block-Initiative Module
-async function AreUsersReady(getGMs) {
-    // check to see if all users are ready
-    let userList = [];
-    for (let i = 0; i < game.users.contents.length; i++) {
-        let user = game.users.contents[i]
-        // if any users are not ready, then we don't want to do anything.
-        if (!(await user.getFlag('block-initiative', 'isReady'))) {
-            return false;
-        }
-        if ((!getGMs && user.hasPlayerOwner)
-            || (getGMs && !user.hasPlayerOwner)) {
-
-            userList.push(user.data.name)
-        }
-    }
-    return true;
-}
-
-// Block-Initiative Module
-async function sendDiscordMessage(message) {
-    $.ajax({
-        method: 'POST',
-        url: game.i18n.localize("BLOCKINITIATIVE.DiscordUrl"),
-        contentType: "application/json",
-        data: message,
-    });
-}
-
-// Block-Initiative Module
-function buildMessage(pingTargets, message) {
-    let messageString = "";
-    for (let i = 0; i < pingTargets.length; i++) {
-        messageString += "<@" + pingTargets[i] + "> "
-    }
-    let messageJSON = {
-        "content": messageString + " " + message
-    }
-
-    let messageReturn = JSON.stringify(messageJSON);
-
-    return messageReturn;
-}
-
-// Block-Initiative Module
-function getPlayersInCombat() {
-    let usersInCombat = [];
-    let combatants = Array.from(game.combat.combatants.values());
-    let actorIDs = [];
-    for (let i = 0; i < combatants.length; i++) {
-        actorIDs.push(combatants[i].data.actorId);
-    }
-    for (let j = 0; j < game.users.contents.length; j++) {
-        let user = game.users.contents[j];
-        if (user.isGM) {
-            usersInCombat.push(user);
-        } else if (!user.isGM && user.character) {
-            let characterID = user.character.id;
-            if (actorIDs.indexOf(characterID) != -1) {
-                usersInCombat.push(user);
-            }
-        }
-    }
-    return usersInCombat;
-}
-
-// Block-Initiative Module
-async function getUserDiscordIDs(getGMs) {
-    let users;
-    if (game.combat) {
-        users = getPlayersInCombat();
-    } else {
-        users = game.users.contents;
-    }
-    let targetUsers = [];
-    for (let i = 0; i < users.length; i++) {
-        let user = users[i];
-        if ((getGMs && !user.hasPlayerOwner) ||
-            (!getGMs && user.hasPlayerOwner)) {
-
-            targetUsers.push(await user.getFlag('block-initiative', 'discordID'));
-        }
-    }
-    return targetUsers;
-}
+})
 
 
